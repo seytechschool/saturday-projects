25725c763ebd4b1aa951a4e8c7f6665df9e14736
Nurseit Kalbaev <kalban1991@gmail.com>
2020-02-17 05:08:02 -0800

    Nurseit Saturday Project off-canvas

--- javascript/off-canvas-menu/script.js
+++ javascript/off-canvas-menu/script.js
@@ -1,12 +1,32 @@
-var open = document.querySelector('.open');
-var close = document.querySelector('.close');
+var navBar = document.querySelector('#nav');
+var aside = document.querySelector('#side');
+var closeIcon = document.querySelector('#close');
+var openIcon = document.querySelector('#open');
 
-open.addEventListener('click', function () {
-    document.querySelector('nav').style.width = '350px';
-    document.querySelector('main').style.marginLeft = '370px';
-});
+closeIcon.addEventListener('click', function(event){
+   // event.target.navBar.style.display = 'none';
+    // aside.style.width = '100%';
+    aside.style.margin ='0 20px 20px 20px';
+    // navBar.style.display = 'none';
+    navBar.style.width = '0';
+    navBar.style.visibility = 'hidden';
+    closeIcon.style.visibility = 'hidden';
+    // aside.style.zIndex = '2';
+    aside.style.transition = 'all .5s'
+    navBar.style.transition = 'all .5s'
+    
+})
 
-close.addEventListener('click', function () {
-    document.querySelector('nav').style.width = '0';
-    document.querySelector('main').style.marginLeft = '20px';
-});
+openIcon.addEventListener('click', function(event) {
+    //event.target.navBar.style.display = 'block';
+    // aside.style.width = '70%';
+    aside.style.margin = '0 20px 20px 280px';
+    // navBar.style.display = 'block';
+    navBar.style.width = '270px';
+    closeIcon.style.visibility = 'visible';
+    navBar.style.visibility = 'visible';
+    aside.style.transition = 'all .5s'
+    // navBar.style.transition = 'all .5s'
+})
+
+
