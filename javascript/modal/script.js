d3fbcda9184e2d2f656ade9b109d1d310500748d
Uzak Kazakbaev <ukazakbaev@gmail.com>
2020-02-17 04:07:04 -0800

    Uzak Kazakbaev

---
+++ javascript/modal/script.js
@@ -0,0 +1,23 @@
+var openButton = document.querySelector('.press');
+var modal = document.querySelector('.modal');
+var closeButton = document.querySelector('#close');
+
+document.addEventListener('click', function(event) {
+    if ( event.target.tagName !== 'BUTTON' &&
+        event.target.closest('.modal') === null)  {
+        modal.style.display ='none';
+    }
+})
+
+openButton.addEventListener('click', function(event) {
+    modal.style.display = 'block'
+})
+closeButton.addEventListener('click', function(event) {
+    modal.style.display = 'none'
+})
+
+// function close() {
+//     modal.style.display = 'none';
+// }
+
+
