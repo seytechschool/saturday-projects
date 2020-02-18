513645edb720f1747a20422cf5e76428b56533cb
DinaDina <59812601+tanashikova@users.noreply.github.com>
2020-02-17 18:24:21 -0800

    off canvas menu by dina

---
+++ javascript/off-canvas-menu/script.js
@@ -0,0 +1,11 @@
+
+function openNav() {
+    document.getElementById("navBar").style.width = "300px";
+    document.getElementById("content").style.marginLeft = "300px";
+  }
+  
+  function closeNav() {
+    document.getElementById("navBar").style.width = "0";
+    document.getElementById("content").style.marginLeft = "0";
+  }
+  
