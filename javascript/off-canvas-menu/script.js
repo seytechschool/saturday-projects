ef51bec953afbb4ba2ab838b9219217d02615256
Uzak Kazakbaev <ukazakbaev@gmail.com>
2020-02-18 03:26:36 -0800

    Saturday Project Off- Canvas-menu

--- javascript/off-canvas-menu/script.js
+++ javascript/off-canvas-menu/script.js
@@ -4,15 +4,11 @@ var closeIcon = document.querySelector('#close');
 var openIcon = document.querySelector('#open');
 
 closeIcon.addEventListener('click', function(event){
-   // event.target.navBar.style.display = 'none';
-    // aside.style.width = '100%';
     aside.style.margin ='0 20px 20px 20px';
     navBar.style.display = 'none'
 })
 
 openIcon.addEventListener('click', function(event) {
-    //event.target.navBar.style.display = 'block';
-    // aside.style.width = '70%';
     aside.style.margin = '0 20px 20px 280px';
     navBar.style.display = 'block'
 })
