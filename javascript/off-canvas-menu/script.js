9af62523ce8f831aed0025fafa7f6a7b5dc5da87
koloebue <aman_samudinov@mail.ru>
2020-02-17 16:07:43 -0800

    Off-canvas menu

--- javascript/off-canvas-menu/script.js
+++ javascript/off-canvas-menu/script.js
@@ -1,12 +1,27 @@
-var open = document.querySelector('.open');
-var close = document.querySelector('.close');
+function openSidebar () {
+    var main = document.querySelector('.main');
+    main.style.position = 'absolute';
+    main.style.top ='0';
+    main.style.left = '30%';
+    main.style.width = '100%';
+    main.style.height = '100vh'
+    main.style.padding = '10px'
 
-open.addEventListener('click', function () {
-    document.querySelector('nav').style.width = '350px';
-    document.querySelector('main').style.marginLeft = '370px';
-});
 
-close.addEventListener('click', function () {
-    document.querySelector('nav').style.width = '0';
-    document.querySelector('main').style.marginLeft = '20px';
-});
+    // position: absolute;
+    // background-color: rgb(231, 225, 236);
+    // left: 30%;
+    // width: 100%;
+    // top: 0;
+    // height: 100vh;
+    // padding: 10px;
+}
+
+function closeSidebar () {
+    var main = document.querySelector('.main')
+    main.style.position = 'fixed';
+    main.style.top = '0';
+    main.style.left = '0';
+    main.style.right = '0';
+    main.style.bottom = '0';
+}
