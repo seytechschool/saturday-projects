e9f173f9c739d30f530484e3c7bf5340e88e69a2
surayya <sura8703@gmail.com>
2020-05-28 17:28:20 -0400

    off-canvas done

--- javascript/off-canvas-menu/script.js
+++ javascript/off-canvas-menu/script.js
@@ -1,12 +1,18 @@
-var open = document.querySelector('.open');
-var close = document.querySelector('.close');
 
-open.addEventListener('click', function () {
-    document.querySelector('nav').style.width = '350px';
-    document.querySelector('main').style.marginLeft = '370px';
-});
+document.querySelector('.open').addEventListener('click', openNav);
 
-close.addEventListener('click', function () {
-    document.querySelector('nav').style.width = '0';
-    document.querySelector('main').style.marginLeft = '20px';
-});
+
+
+function openNav() {
+    console.log("hamburger clicked")
+    document.querySelector('nav').style.width = '257px';
+    document.querySelector('main').style.marginLeft = "377px";
+}
+
+document.querySelector('.close').addEventListener('click', closeNav);
+
+function closeNav() {
+    document.querySelector('nav').style.width = "0px";
+    document.querySelector('main').style.marginLeft = "13px";
+}
+
